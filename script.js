c63cedca2ece81174a4338d1cb3d16a277759c1b
rum-n <manev.ru@gmail.com>
2020-01-02 23:47:35 +0200

    layout and styles

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){$("body").hide().fadeIn(2000);});
 var textWrapper = document.querySelector('.ml11 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({loop: false})
   .add({
     targets: '.ml11 .line',
     scaleY: [0,1],
